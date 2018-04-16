5f4cd183f37bcbee4a8574a8c19e8b9101272c3d
Jeongsoo Park <toracle@gmail.com>
2018-04-16 09:45:29 +0900

    run crawler by choice

---
+++ kasr/sermons/crawler.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+from .crawlers.base import Crawler
+from .crawlers.pyeongan import PyeonganArticleList
+from .crawlers.buksuwon import BuksuwonArticleList
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dataset', nargs='*')
+    args = parser.parse_args()
+
+    article_class_id_to_class = {
+        'pyeongan': PyeonganArticleList,
+        'buksuwon': BuksuwonArticleList,
+    }
+
+    if args.dataset:
+        article_list_class = [article_class_id_to_class.get(dataset_name) for dataset_name in args.dataset if dataset_name is not None]
+    else:
+        article_list_class = article_class_id_to_class.values()
+
+    for list_class in article_list_class:
+        crawler = Crawler(list_class)
+        crawler.fetch()
