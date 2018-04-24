cb6ca8b7f39960e0ad293d6adf44809223cfeb6f
Jeongsoo Park <toracle@gmail.com>
2018-04-25 08:13:21 +0900

    refactor, update preprocess logics

--- kasr/sermons/core.py
+++ kasr/sermons/core.py
@@ -13,6 +13,13 @@ from sklearn.manifold import TSNE
 matplotlib.rc('font', family='Noto Sans CJK KR')
 
 
+def ensure_dir(path, parent=False):
+    _path = os.path.dirname(path) if parent else path
+
+    if not os.path.isdir(_path):
+        os.makedirs(_path)
+
+
 class SentenceReader(object):
     def __init__(self, path):
         self.path = path
@@ -31,11 +38,9 @@ class SermonVoca(object):
         self.name = name
 
     def make(self):
-        if not os.path.isdir('outputs'):
-            os.makedirs('outputs')
-
-        if not os.path.isdir('models'):
-            os.makedirs('models')
+        ensure_dir('outputs')
+        ensure_dir('models')
+        ensure_dir('words')
 
         self.make_corpus()
         self.make_model()
@@ -62,8 +67,9 @@ class SermonVoca(object):
                             _l = self.clean_punctuation(l)
                             if not _l:
                                 continue
-                            sentence = ['{}/{}'.format(word, tag) for word, tag in processor.pos(_l) if self.filter_tag(tag)]
-                            fout.write(' '.join(sentence) + '\n')
+                            sentence = ['{}/{}'.format(word, tag) for word, tag in processor.pos(_l) if self.filter_tag(tag) and self.filter_word(word)]
+                            if len(sentence) > 2:
+                                fout.write(' '.join(sentence) + '\n')
 
     def make_model(self):
         sentences_vocab = SentenceReader('outputs/sermon-{}-corpus.txt'.format(self.name))
@@ -73,15 +79,26 @@ class SermonVoca(object):
         self.model.build_vocab(sentences_vocab)
         self.model.train(sentences_train, total_examples=self.model.corpus_count, epochs=self.model.epochs)
         self.model.save('models/{}'.format(self.name))
+        self.save_words(self.model, 'words/{}'.format(self.name))
 
     def filter_tag(self, tag):
-        return tag not in set(['J', 'E', 'X'])
+        return tag not in set(['J', 'E', 'X', 'S'])
+
+    def filter_word(self, word):
+        try:
+            float(word)
+            return False
+        except ValueError:
+            if len(word) > 1:
+                return True
+            return False
 
     def clean_punctuation(self, line):
         return line.replace('"', '').replace("'", '').replace(',', '').\
             replace('....', '').replace('...', '').replace('..', '').\
             replace('(', ' ').replace(')', ' ').replace('?', '').replace('!', '').\
             replace('“', '').replace('”', '').replace(chr(8216), '').replace(chr(8217), '').\
+            replace('…', '').\
             strip()
 
     def load(self):
@@ -90,6 +107,11 @@ class SermonVoca(object):
 
     def most_similar(self, positive=None, negative=None, topn=10):
         return self.model.wv.most_similar(positive=positive, negative=negative, topn=topn)
+
+    def save_words(self, model, path):
+        with open(path, 'w') as fout:
+            for word in model.wv.vocab:
+                fout.write('{}\n'.format(word))
 
     def tsne_plot(self):
         labels = []
